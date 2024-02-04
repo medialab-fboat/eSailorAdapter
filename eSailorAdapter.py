be5ce16404d4834a6bdb4b5c6e526ad8340606b9
Dickson Henrique Justino Daniel <ckrique@hotmail.com>
2024-02-04 00:47:42 -0300

    Revert "Including electricPropulsionPower reading"
    This reverts commit b468f90cf4f4de172ca32b76bb23da14af59e56f.

--- eSailorAdapter.py
+++ eSailorAdapter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import time
 import rospy
-from mavros_msgs.msg import OverrideRCIn, WaypointList, Waypoint, State, RCOut
+from mavros_msgs.msg import OverrideRCIn, WaypointList, Waypoint, State
 from mavros_msgs.srv import *
 from std_msgs.msg import Int32MultiArray
 from std_msgs.msg import Float64MultiArray, Float64
@@ -64,7 +64,7 @@ class PixhawkDataTopicManager:
 
         #IMU
         self.orientation = None
-        self.imu_topic = '/mavros/imu/data'
+        self.imu_topic = '/mavros/imu/data'        
         rospy.Subscriber(self.imu_topic, Imu, self.imu_callback)
         
 
@@ -81,10 +81,7 @@ class PixhawkDataTopicManager:
         rospy.Subscriber("/mavros/global_position/compass_hdg", Float64, self.handle_compass_hdg_rad)
 
 
-        #Pixhawk Channels
-        self.rcIn_topic = '/mavros/rc/out'
-        rospy.Subscriber(self.rcIn_topic, RCOut, self.rc_callback)
-        
+
         
         self.distanceToTarget = 0
         self.angleBetweenFowardAndTarget = 0
@@ -93,7 +90,7 @@ class PixhawkDataTopicManager:
         self.apparentWindAngle = 1.5
         self.boomAngle = 1.6
         self.rudderAngle = 1.7
-        self.electricPropulsionPower = 0
+        self.electricPropulsionPower = 1.8
         self.rollAngle = 0
 
 
@@ -118,10 +115,6 @@ class PixhawkDataTopicManager:
         self.current_position = position
         self.calculate_distance()
 
-    def rc_callback(self, data):
-        #print("PWM of channel 1:", data.channels[0])        
-        self.electricPropulsionPower = int(data.channels[0])
-
     def handle_compass_hdg_rad(self, data):
         self.current_yaw = data.data        
 
@@ -145,10 +138,10 @@ class PixhawkDataTopicManager:
                 vehicle_lat = self.current_position.latitude
                 vehicle_lon = self.current_position.longitude
 
-                #rospy.loginfo(f'Latitude waypoint{waypoint_lat}')
-                #rospy.loginfo(f'Longitude waypoint{waypoint_lon}')
-                #rospy.loginfo(f'Latitude veiculo{vehicle_lat}')
-                #rospy.loginfo(f'Longitude veiculo{vehicle_lon}')
+                rospy.loginfo(f'Latitude waypoint{waypoint_lat}')
+                rospy.loginfo(f'Longitude waypoint{waypoint_lon}')
+                rospy.loginfo(f'Latitude veiculo{vehicle_lat}')
+                rospy.loginfo(f'Longitude veiculo{vehicle_lon}')
 
                 self.distanceToTarget = self.calculate_distance_between_points(waypoint_lat, waypoint_lon, vehicle_lat, vehicle_lon)
                 
